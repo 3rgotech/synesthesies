fa0898372649a91651529f6600bbb17ce29c7798
Romain GONÇALVES <romain.goncalves@gmail.com>
2024-01-17 12:30:24 +0100

    Adds model to store subject data

---
+++ app/Models/Subject.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use App\Enum\Disorder;
+use Illuminate\Database\Eloquent\Casts\AsEnumCollection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'email',
+        'gender',
+        'birth_year',
+        'citizenship',
+        'region',
+        'language',
+        'keep_informed',
+        'disorders',
+        'diagnosis',
+        'always_existed',
+        'has_changed',
+        'problematic',
+        'comments',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'keep_informed'  => 'boolean',
+        'disorders'      => AsEnumCollection::class . ':' . Disorder::class,
+        'always_existed' => 'boolean',
+        'has_changed'    => 'boolean',
+        'problematic'    => 'boolean',
+    ];
+}
---
+++ database/migrations/2024_01_17_110742_create_subjects_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('gender');
+            $table->unsignedSmallInteger('birth_year');
+            $table->string('citizenship');
+            $table->string('region');
+            $table->string('language');
+            $table->boolean('keep_informed')->default(false);
+            $table->json('disorders');
+            $table->string('diagnosis');
+            $table->boolean('always_existed')->nullable();
+            $table->boolean('has_changed')->nullable();
+            $table->boolean('problematic')->nullable();
+            $table->text('comments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
