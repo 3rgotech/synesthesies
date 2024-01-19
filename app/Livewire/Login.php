b6424a3ea2fdb2762237498182aa3f534e9c8963
Romain GONÇALVES <romain.goncalves@gmail.com>
2024-01-19 17:15:58 +0100

    Login with magic link

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -2,9 +2,14 @@
 
 namespace App\Livewire;
 
+use App\Mail\LoginLink;
 use App\Models\Subject;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
+use MagicLink\Actions\LoginAction;
+use MagicLink\MagicLink;
 
 class Login extends Component
 {
@@ -12,12 +17,26 @@ class Login extends Component
     public string $error   = '';
     public string $success = '';
 
+    public function mount()
+    {
+        if (Auth::guard('subjects')->check()) {
+            return redirect()->route('test-list');
+        }
+    }
+
     public function submit()
     {
         $subject = Subject::where('email', $this->email)->first();
         if (blank($subject)) {
             $this->error = 'Cette adresse email n\'existe pas dans notre base de données de participants.';
         } else {
+            $action = new LoginAction($subject);
+            $action->guard('subjects');
+            $action->remember();
+
+            $url = MagicLink::create($action, 60, 1)->url; // 60 minutes, single use
+            Mail::to($subject->email)->send(new LoginLink($url));
+
             $this->error = '';
             $this->success = 'Un lien de connexion vient de vous être envoyé à l\'adresse indiquée.';
             $this->email = '';
